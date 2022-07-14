af4a1eb4e2e7df265e0968d53db0a0b7fa380147
DanielRozanowski <81776490+DanielRozanowski@users.noreply.github.com>
2022-07-14 22:27:40 +0200

    JUnit.betterVerison1

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,31 +6,5 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args){
 
-       /* SimpleUser user = new SimpleUser("theForumUser");
-
-        String result = user.getUserName();
-
-        if(result.equals("theForumUser")){
-            System.out.println("testOK");
-        } else {
-            System.out.println("error?");
-        } */
-
-        Calculator numbers = new Calculator(10, 20);
-
-        Integer result = numbers.add();
-        Integer result2 = numbers.subtract();
-
-        if(result.equals(30)){
-            System.out.println("OK");
-        } else {
-            System.out.println("Not OK");
-        }
-
-        if(result2.equals(10)){
-            System.out.println("OK");
-        } else {
-            System.out.println("Not OK");
-        }
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/ForumTestSuite.java
@@ -2,9 +2,16 @@ package com.kodilla.testing.forum;
 
 import com.kodilla.testing.user.SimpleUser;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Forum Test Suite")
 public class ForumTestSuite {
+
+    @DisplayName(
+            "When created SimpleUser with name, " +
+             "then getUsername should return correct name"
+    )
 
     @Test
     void testCaseUsername(){
